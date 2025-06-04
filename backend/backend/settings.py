22f4547efb011c490f5bd32d75a6ac8ca1b6cc67
Daniel Lin <dlin0505@berkeley.edu>
2025-06-03 22:41:01 -0600

    Revert "prepare for deployment"
    This reverts commit ce01dd6e70a4e06520c213d65451b9ac02f7877e.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+
 from dotenv import load_dotenv
 import os
 
@@ -96,12 +97,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PWD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
